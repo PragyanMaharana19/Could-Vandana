d620e7bc7f44063e43963b30b841a3db4d0664c2
Pragyan Maharana <148577185+PragyanMaharana19@users.noreply.github.com>
2023-10-21 22:22:40 +0530

    Update ShuffleArray.java

--- ShuffleArray.java
+++ ShuffleArray.java
@@ -16,7 +16,7 @@ public class ShuffleArray {
 	        int length = array.length;
 
 	        for (int i = length - 1; i > 0; i--) {
-	            // Generate an index between 0 and i (inclusive)
+	            
 	            int randomIndex = (int) (Math.random() * (i + 1));
 
 	            // Swap the elements at randomIndex and i
